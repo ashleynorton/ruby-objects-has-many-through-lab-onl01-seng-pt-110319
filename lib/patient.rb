2d4c9f3fdb96b9d1ff6bbcf0c85045935e1f57d2
Ashley Norton <nortonashleym@gmail.com>
2020-01-05 00:39:04 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,11 +17,15 @@ class Patient
   end 
   
   def appointments
-    
+    Appointment.all.select do |appointment|
+      appointment.patient == self 
+    end 
   end 
   
   def doctors
-    
+    appointments.map do |appointment|
+      appointment.doctor 
+    end 
   end 
   
 end 
